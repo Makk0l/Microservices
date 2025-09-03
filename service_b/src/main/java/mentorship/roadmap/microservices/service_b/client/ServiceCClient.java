f4d30556a20efbd71c73017eade696cdf21cb1c1
Makkol <timur.shahmadedov@yandex.ru>
2025-09-03 12:03:13 +0300

    beta-version

--- service_b/src/main/java/mentorship/roadmap/microservices/service_b/client/ServiceCClient.java
+++ service_b/src/main/java/mentorship/roadmap/microservices/service_b/client/ServiceCClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-@FeignClient(name = "service-c", url = "{service-c.url}")
+@FeignClient(name = "service-c", url = "${service-c.url}")
 public interface ServiceCClient {
 
     @PostMapping("/api/save")
